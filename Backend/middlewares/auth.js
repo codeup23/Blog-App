2d1e1ef2704ae5c6080b273c6e7aeb9afa8b5c54
Utkarsh Pandey <108897672+codeup23@users.noreply.github.com>
2024-07-03 09:13:48 +0530

    Update auth.js

--- Backend/middlewares/auth.js
+++ Backend/middlewares/auth.js
@@ -4,12 +4,12 @@ require('dotenv');
 exports.auth = (req, res, next) => {
   const token = req.cookies.token;
   if (!token) {
-    return res.status(401).json('Unauthorized');
+    return res.status(401).json('Unauthorized, login first');
   }
 
   jwt.verify(token, process.env.SECRET, {}, (err, user) => {
     if (err) {
-      return res.status(401).json('Unauthorized');
+      return res.status(401).json('Unauthorized user detected');
     }
     req.user = user;
     next();
